6ba9929c910043ef984ff36dc1bde05f6df31c85
Thomas Léger <El-Fitz@users.noreply.github.com>
2017-03-11 23:47:14 +0100

    Publishing function to Github

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+var dns = require ( 'dns' )
+
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+  let website = event.company.website;
+  extractWebsiteDomain(website, function(err, fullDomain) {
+    if (typeof fullDomain !== null && fullDomain !== null) {
+      checkAvailable(website, function(err, addresses) {
+        if (err || addresses === null || typeof addresses == 'undefined' || addresses.length === 0) {
+          console.log("URL didn't resolve");
+          event.websiteDomainIsValid = false;
+        } else {
+          event.domainARecords = addresses;
+          event.websiteDomainIsValid = true;
+        }
+        context.done(null, event);
+      });
+    } else {
+      context.done();
+    }
+  });
+};
+
+function checkAvailable(url, done) {
+  //uses the core modules to run an IPv4 resolver that returns 'err' on error
+  dns.resolve4( url, done(err, addresses));
+}
+
+function extractWebsiteDomain(website, done) {
+  var params = {
+    FunctionName: 'extractWebsiteDomainAndTLD',
+    InvocationType: 'RequestResponse',
+    LogType: 'Tail',
+    Payload: '{"website" : ' + JSON.stringify(website) + '}'
+  };
+
+  lambda.invoke(params, function(err, data) {
+    if (err) {
+      console.log("Err: ", err);
+      done(err, null);
+    } else {
+      let info = JSON.parse(data.Payload);
+      done(err, info.fullDomain);
+    }
+  });
+}
